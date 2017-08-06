61ca2faa3a84cb3e6a236d49cd7aec706a3bb0a5
Austin Davis <adavisdesigns25@gmail.com>
2017-08-05 22:54:51 -0400

    initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,48 @@
+const React = require('react');
+const WeatherForm = require('WeatherForm');
+const WeatherMessage = require('WeatherMessage');
+const openWeatherMap = require('openWeatherMap');
+
+var Weather = React.createClass({
+    getInitialState: function  () {
+        return {
+            isLoading: false
+        };
+    },
+    handleSearch: function (location) {
+        var that = this;
+
+        this.setState({isLoading: true});
+        openWeatherMap.getTemp(location).then(function (temp) {
+            that.setState({
+                location: location,
+                temp: temp,
+                isLoading: false
+            });
+        }, function (errorMessage) {
+            that.setState({isLoading: false});
+            alert(errorMessage);
+        });
+    },
+    render: function () {
+        //es6 destructuring
+        var {isLoading, temp, location} = this.state;
+
+        function renderMessage() {
+            if (isLoading) {
+                return <h3>Fetching weather...</h3>;
+            } else if (temp && location) {
+                return <WeatherMessage temp={temp} location={location}/>;
+            }
+        }
+        return (
+            <div>
+                <h3>Weather component</h3>
+                <WeatherForm onSearch={this.handleSearch}/>
+                {renderMessage()}
+            </div>
+        );
+    }
+});
+
+module.exports = Weather;
---
+++ app/components/WeatherForm.jsx
@@ -0,0 +1,30 @@
+const React = require('react');
+
+var WeatherForm = React.createClass({
+    onFormSubmit: function (e) {
+        e.preventDefault();
+        //get value off of location
+            //sets the location var
+        var location = this.refs.location.value;
+        //check if user input is valid
+        if (location.length > 0) {
+            this.refs.location.value = '';
+            //pass in location user searched for
+            this.props.onSearch(location);
+        }
+    },
+    render: function () {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <div>
+                        <input type="text" placeholder="Enter city name" ref="location"/>
+                        <button>Get Weather</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+});
+
+module.exports = WeatherForm;
