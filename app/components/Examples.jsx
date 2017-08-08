553137491e8b196973c6765fa6cfab8c868af878
Austin Davis <adavisdesigns25@gmail.com>
2017-08-08 11:21:49 -0400

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,7 +11,7 @@ const React = require('react');
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples component!</h3>
+            <h3>Example</h3>
             <p>Welcome to examples page.</p>
         </div>
     )
