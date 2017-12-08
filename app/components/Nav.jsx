e07991c50cff6fd35115828980accfa936d0c573
Austin Davis <adavisdesigns25@gmail.com>
2017-12-08 13:48:07 -0500

    add foundation  & jQuery

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -16,13 +16,24 @@ const {Link, IndexLink} = require('react-router');
 
 var Nav = (props) => {
     return (
-        <div>
-            <h2>Nav component</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather | </IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About | </Link>
-            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li>
+                        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                    </li>
+                    <li>
+                        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+            </div>
         </div>
-    )
+    );
 };
 
 module.exports = Nav;
