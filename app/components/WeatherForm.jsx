268ba709f18912d995606bbbf913e704e152fd82
Austin Davis <adavisdesigns25@gmail.com>
2018-02-23 17:01:00 -0500

    added styles to weather page

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -19,7 +19,7 @@ var WeatherForm = React.createClass({
                 <form onSubmit={this.onFormSubmit}>
                     <div>
                         <input type="text" placeholder="Enter city name" ref="location"/>
-                        <button>Get Weather</button>
+                        <button className="button expanded hollow">Get Weather</button>
                     </div>
                 </form>
             </div>
